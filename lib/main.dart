fc2537877161f2e67057ceffa1927f010860ad1f
Wei-Chen Lin <36478352+jake-08@users.noreply.github.com>
2020-10-20 08:59:48 +1100

    Add location route

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ void main() => runApp(MaterialApp(
     '/': (context) => Loading(),
     '/home': (context) => Home(),
     '/location': (context) => ChooseLocation(),
+    '/location': (context) => ChooseLocation(),
   }
 ));
 
